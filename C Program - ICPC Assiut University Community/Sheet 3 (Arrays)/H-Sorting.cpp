2393dfa8a3be2269d8cffde1dc7cb741eac9a9bc
hshanto707 <hshanto707@gmail.com>
2022-07-11 15:19:41 +0600

    Sheet 3 - H

---
+++ C Program - ICPC Assiut University Community/Sheet 3 (Arrays)/H-Sorting.cpp
@@ -0,0 +1,25 @@
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int n; cin >> n;
+  int arr[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  sort(arr, arr + n);
+
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << ' ';
+  }
+
+  cout << '\n';
+
+  return 0;
+}
